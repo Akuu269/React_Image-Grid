c54ce8e00399f542e17b28a43a4d8430de1138c7
Tom Lynch <Tom Lynch>
2018-01-20 08:51:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      pictures: [],
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='+process.env.REACT_APP_API_KEY+'&tags=nyc&per_page=10&page=1&format=json&nojsoncallback=1')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(j){
+      let picArray = j.photos.photo.map((pic) => {
+        var srcPath = 'https://farm'+pic.farm+'.staticflickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
+        return(
+          <img alt="dogs" src={srcPath}></img>
+        )
+      })
+      this.setState({pictures: picArray});
+    }.bind(this))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          {this.state.pictures}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
