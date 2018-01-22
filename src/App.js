9a5e216e51637dec47c2aa0e5b0675795b5774e7
Tom Lynch <tlynch001@yahoo.com>
2018-01-22 13:46:48 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -12,12 +12,15 @@ class App extends Component {
   }
 
   componentDidMount(){
+    alert(process.env.REACT_APP_API_KEY);
     fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='+process.env.REACT_APP_API_KEY+'&tags=nyc&per_page=10&page=1&format=json&nojsoncallback=1')
     .then(function(response){
       return response.json();
     })
     .then(function(j){
+      alert(JSON.stringify(j));
       let picArray = j.photos.photo.map((pic) => {
+        
         var srcPath = 'https://farm'+pic.farm+'.staticflickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
         return(
           <img alt="dogs" src={srcPath}></img>
